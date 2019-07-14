e7ea2c15fd24e1ce53824dcf64f29ca120088bf4
HiDoYa <hiroyagojo@gmail.com>
2019-07-14 11:44:27 -0700

    Updated scripts

--- utilities/image-process.py
+++ utilities/image-process.py
@@ -1,7 +1,15 @@
 import cv2
 import numpy
 import sys
+import os
+
+os.mkdir('images')
 
 # Passed in all images as arg
+img_names = sys.argv[1:]
 
-image = cv2.imread('')
+for idx, img_name in enumerate(img_names):
+    image = cv2.imread(img_name)
+    image = cv2.resize(image, (128, 128))
+
+    cv2.imwrite("images/" + str(idx) + ".jpg", image)
