9550338e10448e7ca582b61a16633214d909551d
HiDoYa <hiroyagojo@gmail.com>
2019-07-14 11:59:44 -0700

    Updated script for conversion errors

--- utilities/image-process.py
+++ utilities/image-process.py
@@ -4,12 +4,17 @@ import sys
 import os
 
 os.mkdir('images')
+errfile  = open("err.txt", "w")
 
 # Passed in all images as arg
 img_names = sys.argv[1:]
 
 for idx, img_name in enumerate(img_names):
-    image = cv2.imread(img_name)
-    image = cv2.resize(image, (128, 128))
+    try:
+        image = cv2.imread(img_name)
+        image = cv2.resize(image, (128, 128))
+        cv2.imwrite("images/" + str(idx) + ".jpg", image)
+    except:
+        errfile.write("Error for file:" + img_name + '\n')
 
-    cv2.imwrite("images/" + str(idx) + ".jpg", image)
+errfile.close()
