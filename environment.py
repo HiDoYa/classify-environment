80cd335199da701506dbe286301097baf49723e3
HiDoYa <hiroyagojo@gmail.com>
2019-07-14 19:54:45 -0700

    Fixed labels to be integer

--- environment.py
+++ environment.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 # Loads images and returns 2 tuples for ((trainData, trainLabels), (testData, testLabels)) 
 # Where data has shape (num_samples, rows, cols, depth) and rows=cols=128 for 128x128 image and depth=3 (for rgb)
 # Note: Assumes image_data_format=channels_last
+# Note: Returns as lists (not as numpy array)
 class Environment:
     @staticmethod
     def load_data():
@@ -19,9 +20,9 @@ class Environment:
         ocean_images = [cv2.imread(file) for file in glob.glob(pathname + "/images/processed/ocean/*.jpg")]
         X = city_images + forest_images + mountain_images + ocean_images
         
-        # Labels
-        y = ["city"] * len(city_images) + ["forest"] * len(forest_images) + \
-            ["mountain"] * len(mountain_images) + ["ocean"] * len(ocean_images)
+        # Labels: 0 = city, 1 = forest, 2 = mountain, 3 = ocean
+        y = [0] * len(city_images) + [1] * len(forest_images) + \
+            [2] * len(mountain_images) + [3] * len(ocean_images)
 
         # Randomly sort and split into train and test sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
